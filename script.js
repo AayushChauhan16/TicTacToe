249f65b3f1f06a04830b7fd7c3ff81c69e011369
AayushChauhan16 <aayushchauhan559@gmail.com>
2026-01-28 13:38:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+"use strict";
+
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;//playerX, playerO
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("Box was clicked");
+        if (turnO) {
+            box.classList.add("O");
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            box.classList.add("X");
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+
+});
+
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+};
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+        box.classList.remove("X", "O");
+    }
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}🎉`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const matchtied = () => {
+    msg.innerText = `Match tied!`;
+    disableBoxes();
+};
+
+const checkWinner = () => {
+    let isWinner = false;
+
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val !== "" && pos1Val === pos2Val && pos2Val === pos3Val) {
+            showWinner(pos1Val);
+            isWinner = true;
+        }
+    };
+
+    
+    let allFilled = true;
+    boxes.forEach((box) => {
+        if (box.innerText === "") {
+            allFilled = false;
+        }
+    });
+
+    if (allFilled && !isWinner) {
+        matchtied();
+        msgContainer.classList.remove("hide");
+    }
+};
+
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
